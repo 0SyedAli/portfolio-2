d25cc1bf17199fd6563b49f7a5ca77f2d8da70be
Syed Ahtisham Ali <111875652+0SyedAli@users.noreply.github.com>
2025-04-15 22:18:16 +0500

    Update header.js

--- components/header.js
+++ components/header.js
@@ -12,7 +12,7 @@ function Header() {
                 <h3 className={classes.header_tag}>front-end developer</h3>
                 <h4 className={classes.header_details}>Front-end developer focused on building webapps with pixel perfect User Interface.</h4>
                 <div className={classes.cta}>
-                    <Button> <a className="resume" href="https://drive.google.com/file/d/18T-5LVYR_-zGjk2V8uBzDPpJZotHyp4Z/view" target='_blank' rel="noopener noreferrer">Download Resume</a> </Button>
+                    <Button> <a className="resume" href="https://drive.google.com/file/d/18GMoBQE4CB52MGRhFhqqrmCKjIj6e1AI/view?usp=sharing" target='_blank' rel="noopener noreferrer">Download Resume</a> </Button>
                 </div>
                 <h3 className={classes.header_contact}> <span>Get in touch </span> <span className={classes.emoji_pointer}>👉</span><span> <a href="mailto:sa8449006@gmail.com" target="_blank" rel="noopener noreferrer" className={classes.header_contact_link}>sa8449006@gmail.com</a> </span> </h3>
             </header>
